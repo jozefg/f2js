c5852b15a49b4ad6207efbeb257d6adef4ce1810
Danny Gratzer <jozefg@cmu.edu>
2015-01-01 02:10:53 -0500

    Added deExp, so close to STG :)

--- src/Language/F2JS/Lift.hs
+++ src/Language/F2JS/Lift.hs
@@ -43,3 +43,29 @@ lambdaLift = \case
   Case e alts -> Case (lambdaLift e) (map (fmap lambdaLift) alts)
   Lam c e -> LetRec [Bind c $ Lam c e] (Var 0)
   where liftB (Bind c e) = Bind c (lambdaLift e)
+
+-- | Convert @F ComplicatedExpr@ into @LetRec [ComplicatedExpr] F (Var 0)@.
+-- This is needed to convert to STG.
+deExp :: Expr -> Expr
+deExp = \case
+  Record rs -> Record $ map (fmap deExp) rs
+  Proj e n -> case deExp e of
+               LetRec bs e' -> LetRec bs (Proj e' n)
+               _ -> Proj e n
+  LetRec bs e -> LetRec (map liftB bs) (deExp e)
+  App l r ->
+    let r' = deExp r
+        l' = deExp l
+    in case deExp r of
+        Lit{} -> mergeApp l' r'
+        Var{} -> mergeApp l' r'
+        _     -> {- TODO, who the hell knows if this works.. -}
+          LetRec [Bind Nothing r'] $ mergeApp (succExpr 1 l') (Var 0)
+  Case e alts -> Case (deExp e) (map (fmap deExp) alts)
+  Var i -> Var i
+  Global n -> Global n
+  Lit l -> Lit l
+  Con t -> Con t
+  PrimOp p -> PrimOp p
+  Lam c e -> Lam c e
+  where liftB (Bind c e) = Bind c (deExp e)
