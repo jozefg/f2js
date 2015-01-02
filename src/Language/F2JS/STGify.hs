24c0d4add54dca9eb733d0813f6435bad826201b
Danny Gratzer <jozefg@cmu.edu>
2015-01-01 22:11:05 -0500

    Added expr2atom. FIXME add real error handling

--- src/Language/F2JS/STGify.hs
+++ src/Language/F2JS/STGify.hs
@@ -11,9 +11,16 @@ lit2slit = \case
   A.Double d -> S.Double d
   A.Bool b   -> S.Bool b
 
+expr2atom :: [Name] -> A.Expr -> S.Atom
+expr2atom _ (A.Lit l) = S.LitAtom (lit2slit l)
+expr2atom _ (A.Global n) = S.NameAtom n
+expr2atom ns (A.Var i) = S.NameAtom (ns !! i)
+expr2atom _ _ = error "expr2atom failure: Tried to convert non-atom"
+
 expr2sexpr :: [Name] -> A.Expr -> Gen Name S.SExpr
 expr2sexpr ns = \case
   A.Var i -> return $ S.Var (ns !! i)
   A.Global n -> return $ S.Var n
   A.Lit l -> return $ S.Lit (lit2slit l)
+  A.App (A.Var i) n -> return $ S.App (ns !! i) [expr2atom ns n]
   A.LetRec bs e -> undefined
