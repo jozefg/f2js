2d06e9748a2a79b638c7892aedca51758907d409
Danny Gratzer <jozefg@cmu.edu>
2015-01-02 15:36:11 -0500

    Added deriving show

--- src/Language/F2JS/Target.hs
+++ src/Language/F2JS/Target.hs
@@ -2,23 +2,29 @@ module Language.F2JS.Target where
 import Language.F2JS.Util
 
 data UpdateFlag = Update | NoUpdate
+                deriving Show
 
 data Decl = Decl { declName :: Name
                  , declClos :: Closure }
+          deriving Show
 
 data Closure = Closure { closFlag :: UpdateFlag
                        , closClos :: [Name]
                        , closArgs :: [Name]
                        , closBoxy :: Either String SExpr }
+             deriving Show
 
 data Lit = String String | Double Double | Record [(Name, Atom)] | Bool Bool
+         deriving Show
 data Atom = NameAtom Name | LitAtom Lit
+          deriving Show
 
 data Pat = LitPat Lit
          | RecordPat [(Name, Name)] -- Bind the field at LNAME -> RNAME
          | WildPat
          | ConPat Tag [Name]
          | BindPat Name
+         deriving Show
 
 data SExpr = Let [Closure] SExpr
            | App Name [Atom]
@@ -28,3 +34,4 @@ data SExpr = Let [Closure] SExpr
            | Lit Lit
            | Var Name
            | Proj SExpr Name
+           deriving Show
