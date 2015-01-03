8b6b21acbbc74afd856ad18408999f75c5924265
Danny Gratzer <jozefg@cmu.edu>
2015-01-03 16:34:34 -0500

    Added a lot of primops to the RTS

--- rts/rts.js
+++ rts/rts.js
@@ -28,3 +28,17 @@ var enter = function(c){
     CURRENT_CLOS = c;
     return c.body();
 }
+
+var mkPrim = function(f){
+    return function(){
+        var r = EVAL_STACK.pop();
+        var l = EVAL_STACK.pop();
+        EVAL_STACK.push(f(l, r));
+        return jumpNext();
+    }
+}
+
+var plusPrim  = mkPrim(function(l, r){return l + r;});
+var minusPrim = mkPrim(function(l, r){return l - r;});
+var multPrim  = mkPrim(function(l, r){return l * r;});
+var divPrim   = mkPrim(function(l, r){return l / r;});
