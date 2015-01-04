6d0909df06ccd8586225c8fd629834a819144030
Danny Gratzer <jozefg@cmu.edu>
2015-01-04 13:45:11 -0500

    Added jsify to pipeline

--- src/Language/F2JS/Pipeline.hs
+++ src/Language/F2JS/Pipeline.hs
@@ -1,11 +1,12 @@
 module Language.F2JS.Pipeline where
 import qualified Language.F2JS.AST as A
-import qualified Language.F2JS.Target as S
+import qualified Language.JavaScript.AST as J
 
 import Language.F2JS.Saturate (saturateDecls)
 import Language.F2JS.Lift (liftDecls)
 import Language.F2JS.Closure (closureConvert)
 import Language.F2JS.STGify (stgify)
+import Language.F2JS.CodeGen (jsify)
 
-pipeline :: [A.Decl] -> [S.Decl]
-pipeline = stgify . closureConvert . liftDecls . saturateDecls
+pipeline :: [A.Decl] -> J.Program
+pipeline = jsify . stgify . closureConvert . liftDecls . saturateDecls
