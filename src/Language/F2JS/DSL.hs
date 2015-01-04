d022a5b9587c6880d36c743cb9f5862ef94249cc
Danny Gratzer <jozefg@cmu.edu>
2015-01-04 14:40:49 -0500

    Added start of a dsl for writing AST

---
+++ src/Language/F2JS/DSL.hs
@@ -0,0 +1,25 @@
+module Language.F2JS.DSL where
+import Data.String
+import Language.F2JS.AST
+import Language.F2JS.Util
+
+instance IsString Name where
+  fromString = Str
+instance IsString Expr where
+  fromString = Global . fromString
+instance IsString Lit where
+  fromString = String
+instance Num Lit where
+  fromInteger = Double . fromInteger
+  (+) = undefined
+  (*) = undefined
+  (-) = undefined
+  abs = undefined
+  signum = undefined
+instance Num Expr where
+  fromInteger = Var . fromInteger
+  (+) = undefined
+  (*) = undefined
+  (-) = undefined
+  abs = undefined
+  signum = undefined
