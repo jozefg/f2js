56ba59fbfd7d4c226dea0d23c82b2b89f2e338ff
Danny Gratzer <jozefg@cmu.edu>
2015-01-06 16:19:33 -0500

    Added a more elaborate test program

--- test/Main.hs
+++ test/Main.hs
@@ -12,12 +12,12 @@ program = [ define "foo" 1 $ lit 1 + 0
           , define "main" 0 $ match "bar" [(conPat (tag 1) 1, 0)]]
 
 factorial :: [Decl]
-factorial = [ jsForeign "print" 0 "console.log"
+factorial = [ jsForeign "print" 1 "console.log"
             , define "factorial" 2 $
               match 0 [ (litPat 0, var 1)
                       , (wildPat,
                          "factorial" # (var 0 - lit 1) # (var 0 * var 1))]
-            , define "main" 0 $ "print" # ("factorial" # lit 5 # lit 1)]
+            , define "main" 0 $ "print" # ("factorial" # lit 2 # ("factorial" # lit 5 # lit 1))]
 
 main :: IO ()
 main = compileProgram factorial CompilerConfig { rtsLoc = "rts/rts.js"
